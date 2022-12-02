34c6b0795e6b684a4169ace2518c7cafab84e32b
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-12-02 09:54:17 -0300

    colocando cifão do real e fixando preço para duas casas decimais

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ pizzaJson.map((item, index) => {
   document.querySelector(".pizza-area").append(pizzaItem)
 
   pizzaItem.querySelector(".pizza-item--img img").src = item.img
-  pizzaItem.querySelector(".pizza-item--price").innerHTML = item.price
+  pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description
 })
