da2341a2e43178ea50da2731f30d37be4fb1d43c
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-12-05 10:23:22 -0300

    atribuindo funcoes e organizando codigo

--- js/main.js
+++ js/main.js
@@ -1,39 +1,62 @@
+const seleciona = (elemento) => document.querySelector(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
+
+const abrirModal = () => {
+  seleciona('.pizzaWindowArea').style.opacity = 0
+  seleciona('.pizzaWindowArea').style.display = 'flex'
+  setTimeout(() => {
+    seleciona('.pizzaWindowArea').style.opacity = 1
+  }, 150)
+}
+
+const fecharModal = () => {
+  seleciona('.pizzaWindowArea').style.opacity = 0
+  setTimeout(() => {
+    seleciona('.pizzaWindowArea').style.display = 'none'
+  }, 500)
+}
+
+const botoesFechar = () => {
+  selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', fecharModal)
+  })
+}
+
+const preencheDadosDasPizzas = (pizzaItem, item) => {
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+}
+
+const preencheDadosModal = (item) => {
+  seleciona('.pizzaBig img').src = item.img
+  seleciona('.pizzaInfo h1').innerHTML = item.name
+  seleciona('.pizzaInfo--desc').innerHTML = item.description
+  seleciona('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+}
+
 pizzaJson.map((item, index) => {
-  // console.log(pizza)
 
-  let pizzaItem = document.querySelector(".models .pizza-item").cloneNode(true);
+  let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
 
-  // console.log(pizzaItem)
+  seleciona('.pizza-area').append(pizzaItem)
 
-  document.querySelector(".pizza-area").append(pizzaItem);
 
-  pizzaItem.querySelector(".pizza-item--img img").src = item.img;
-  pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
-  pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
-  pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
+  preencheDadosDasPizzas(pizzaItem, item)
 
-  pizzaItem.querySelector(".pizza-item a").addEventListener('click', (event) => {
-    event.preventDefault();
-    // console.log("clicou na pizza")
+  pizzaItem.querySelector('.pizza-item a').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('Clicou na pizza')
 
-    document.querySelector(".pizzaWindowArea").style.display = 'flex';
 
-    document.querySelector(".pizzaBig img").src = item.img;
-    document.querySelector(".pizzaInfo h1").innerHTML = item.name;
-    document.querySelector(".pizzaInfo--desc").innerHTML = item.description;
-    document.querySelector(".pizzaInfo--actualPrice").innerHTML = `R$ ${item.price.toFixed(2)}`;
+    abrirModal()
+
+
+    preencheDadosModal(item)
 
   })
 
-  document.querySelector(".pizzaInfo--cancelButton ").addEventListener('click', () => {
-
-    document.querySelector(".pizzaWindowArea").style.display = 'none';
-  })
-
-  document.querySelector(".pizzaInfo--cancelMobileButton ").addEventListener('click', () => {
-
-    document.querySelector(".pizzaWindowArea").style.display = 'none';
-  })
-
+  botoesFechar()
 
 })
